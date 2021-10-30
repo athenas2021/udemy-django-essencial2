2ce3cca528024148f9d88bfef80d61c80f07bd8b
athenas2021 <lucas.kano@outlook.com>
2021-10-30 11:32:24 -0300

    Forms..

---
+++ core_app/forms.py
@@ -0,0 +1,7 @@
+from django import forms
+
+class ContatoForm(forms.Form):
+    nome = forms.CharField(label='Nome', max_length=100)
+    contato = forms.EmailField(label='E-email', max_length=100)
+    assunto = forms.CharField(label='Assunto', max_length=100)
+    mensagem = forms.CharField(label='Mensagem', widget=forms.Textarea())
--- core_app/views.py
+++ core_app/views.py
@@ -1,10 +1,29 @@
 from django.shortcuts import render
+from django.contrib import messages
+from .forms import ContatoForm
 
 def index(request):
     return render(request, 'index.html')
 
 def contato(request):
-    return render(request,'contato.html')
+    form = ContatoForm(request.POST or None)
+    if str(request.method) == 'POST':
+        if form.is_valid():
+            nome = form.cleaned_data['nome']
+            email = form.cleaned_data['email']
+            assunto = form.cleaned_data['assunto']
+            mensagem = form.cleaned_data['mensagem']
+
+            print('Nome: ',nome,'E-mail: ', email,'Assunto: ', assunto,'Mensagem: ', mensagem)
+
+            messages.success(request, 'E-mail enviado.') 
+            form = ContatoForm()   
+        else:
+            messages.error(request,'Algo deu errado.')
+    context = {
+        'form' : form
+    }
+    return render(request,'contato.html', context)
 
 def produto(request):
     return render(request, 'produto.html')

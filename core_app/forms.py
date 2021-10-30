fbfe54bdd1accb1f4cfd270b5105162fefb06216
athenas2021 <lucas.kano@outlook.com>
2021-10-30 11:34:23 -0300

    Correcao forms

--- core_app/forms.py
+++ core_app/forms.py
@@ -2,6 +2,6 @@ from django import forms
 
 class ContatoForm(forms.Form):
     nome = forms.CharField(label='Nome', max_length=100)
-    contato = forms.EmailField(label='E-email', max_length=100)
+    email = forms.EmailField(label='E-email', max_length=100)
     assunto = forms.CharField(label='Assunto', max_length=100)
     mensagem = forms.CharField(label='Mensagem', widget=forms.Textarea())
